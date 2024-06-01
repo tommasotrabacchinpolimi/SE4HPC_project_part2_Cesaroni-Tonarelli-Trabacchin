d8402b78a1edf3aad2371b2b7bc76a19b03ca9db
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-06-01 19:09:11 +0200

    Fixed job.sh script

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH --time=00:15:00
 #SBATCH --account=tra24_sepolimi
-#SBATCH --partition=cpu
-#SBATCH --qos=default
 #SBATCH --nodes=2
 #SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+
 
 module load profile/advanced
 srun singularity exec container.sif /app/build/test_multiplication
