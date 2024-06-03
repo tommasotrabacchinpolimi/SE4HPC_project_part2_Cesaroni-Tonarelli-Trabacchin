26a8989b041c8561fa3978cd8cadecf5cc02107e
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-06-03 13:48:22 +0200

    Fixed job.sh file to allow correct execution on cluster

--- job.sh
+++ job.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -l
 #SBATCH --time=00:15:00
 #SBATCH --account=tra24_sepolimi
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --partition=g100_usr_prod
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+export SINGULARITYENV_TMPDIR=$TMPDIR
+module load profile/base
+srun exec --bind $TMPDIR:$TMPDIR SWENG-Ces-Ton-Trab/container.sif bash -c "export OMPI_MCA_tmpdir_base=$TMPDI$
 
 
-module load profile/advanced
-srun singularity exec SWENG-Ces-Ton-Trab/container.sif /app/build/test_multiplication
+
